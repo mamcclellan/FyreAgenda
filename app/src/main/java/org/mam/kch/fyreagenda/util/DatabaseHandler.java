91b052dd4ca6dc3fb25c5f561886fd606a2bf92c
Michael McClellan <mccln89@gmail.com>
2016-12-13 01:16:52 -0600

    Added the SQLite Database Handler and verified that it can successfully store tasks, but I have left it pretty much unnattached since there are about a billion things to fix before it's fully usable. At least for now, it successfully stores the default development tasks and can load them - but no adding new/updating/removing other tasks at the moment.

---
+++ app/src/main/java/org/mam/kch/fyreagenda/util/DatabaseHandler.java
@@ -0,0 +1,159 @@
+package org.mam.kch.fyreagenda.util;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "taskManager";
+    private static final String TABLE_TASKS = "tasks";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_DETAILS = "details";
+    private static final String KEY_CREATION_TIME = "creation_time";
+    private static final String KEY_COMPLETION_TIME = "completion_time";
+    private static final String KEY_TASK_TYPE = "task_type";
+    private static final String KEY_TASK_COMPLETE = "task_complete";
+    private static final String KEY_EDITED = "task_edited";
+    private static final String KEY_NEW_TASK_TYPE = "new_task_type";
+
+
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "CREATE TABLE " + TABLE_TASKS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_DETAILS + " TEXT," + KEY_CREATION_TIME + " INTEGER,"
+                + KEY_COMPLETION_TIME + " INTEGER," + KEY_TASK_TYPE + " INTEGER,"
+                + KEY_TASK_COMPLETE + " BOOLEAN," + KEY_EDITED + " BOOLEAN,"
+                + KEY_NEW_TASK_TYPE + " BOOLEAN"
+                + ")";
+        db.execSQL(create);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASKS);
+        onCreate(db);
+    }
+
+    // Add new taskItem
+    public void addTask(Task.TaskItem taskItem) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, taskItem.getName());
+        values.put(KEY_DETAILS, taskItem.getDetails());
+        values.put(KEY_CREATION_TIME, taskItem.getCreationTime());
+        values.put(KEY_COMPLETION_TIME, taskItem.getCompletionTime());
+        values.put(KEY_TASK_TYPE, taskItem.getTaskTypeValue());
+        values.put(KEY_TASK_COMPLETE, taskItem.getTaskComplete());
+        values.put(KEY_EDITED, taskItem.isEdited());
+        values.put(KEY_NEW_TASK_TYPE, taskItem.isEdited());
+
+        db.insert(TABLE_TASKS, null, values);
+        db.close();
+    }
+
+    // Update taskItem
+    public int updateTask(Task.TaskItem taskItem) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, taskItem.getName());
+        values.put(KEY_DETAILS, taskItem.getDetails());
+        values.put(KEY_CREATION_TIME, taskItem.getCreationTime());
+        values.put(KEY_COMPLETION_TIME, taskItem.getCompletionTime());
+        values.put(KEY_TASK_TYPE, taskItem.getTaskTypeValue());
+        values.put(KEY_TASK_COMPLETE, taskItem.getTaskComplete());
+        values.put(KEY_EDITED, taskItem.isEdited());
+        values.put(KEY_NEW_TASK_TYPE, taskItem.isEdited());
+
+        int result = db.update(TABLE_TASKS, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(taskItem.getID()) });
+        db.close();
+        return result;
+    }
+
+    // Delete taskItem
+    public void deleteTask(Task.TaskItem taskItem) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_TASKS, KEY_ID + " = ?",
+                new String[] { String.valueOf(taskItem.getID()) });
+        db.close();
+    }
+
+    // Retrieve all taskItems
+    public boolean loadTasks() {
+        // dirty way to check if data was already loaded (e.g. onCreate() called after
+        // app has already started
+        if (Task.THISWEEK.size() > 0
+                || Task.NEXTWEEK.size() > 0
+                || Task.THISMONTH.size() > 0
+                || Task.ARCHIVE.size() > 0) return true;
+
+        String selectQuery = "SELECT * FROM " + TABLE_TASKS;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Task.TaskItem taskItem = new Task.TaskItem();
+                // Strings are in order of sql table declaration
+                taskItem.setID(cursor.getString(0));
+                taskItem.setName(cursor.getString(1));
+                taskItem.setDetails(cursor.getString(2));
+                taskItem.setCreationTime(cursor.getLong(3));
+                taskItem.setCompletionTime(cursor.getLong(4));
+                if (cursor.getInt(6) == 1) taskItem.setTaskComplete(true);
+                else taskItem.setTaskComplete(false);
+                if (cursor.getInt(7) == 1) taskItem.setEdited(true);
+                else taskItem.setEdited(false);
+                if (cursor.getInt(8) == 1) taskItem.setNewTaskType(true);
+                else taskItem.setNewTaskType(false);
+                int taskTypeValue = cursor.getInt(5);
+                switch(taskTypeValue) {
+                    case 0:
+                        taskItem.setTaskType(Task.TaskType.THISWEEK);
+                        break;
+                    case 1:
+                        taskItem.setTaskType(Task.TaskType.NEXTWEEK);
+                        break;
+                    case 2:
+                        taskItem.setTaskType(Task.TaskType.THISMONTH);
+                        break;
+                    case 3:
+                        taskItem.setTaskType(Task.TaskType.ARCHIVED);
+                        break;
+                    default:
+                        taskItem.setTaskType(Task.TaskType.ARCHIVED);
+                }
+                Task.addItem(taskItem);
+
+            } while (cursor.moveToNext());
+        }
+        else {
+            // We did not find any saved data
+            cursor.close();
+            db.close();
+            return false;
+        }
+        // We found saved data
+        cursor.close();
+        db.close();
+        return true;
+    }
+
+
+}
--- app/src/main/java/org/mam/kch/fyreagenda/util/Task.java
+++ app/src/main/java/org/mam/kch/fyreagenda/util/Task.java
@@ -1,5 +1,6 @@
 package org.mam.kch.fyreagenda.util;
 
+import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -8,12 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Helper class for providing sample name for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
+
 public class Task {
 
     public enum TaskType {THISWEEK(0, "This Week"), NEXTWEEK(1, "Next Week"), THISMONTH(2, "This Month"), ARCHIVED(3, "Archived");
@@ -38,6 +34,7 @@ public class Task {
     public static final ArrayList<TaskItem> NEXTWEEK = new ArrayList<TaskItem>();
     public static final ArrayList<TaskItem> THISMONTH = new ArrayList<TaskItem>();
     public static final ArrayList<TaskItem> ARCHIVE = new ArrayList<TaskItem>();
+    private static DatabaseHandler database;
 
     /**
      * A map of sample (dummy) items, by ID.
@@ -47,32 +44,32 @@ public class Task {
     private static int COUNT = 0;
 
 
-
-    static {
-        // Add some sample items.
-        // This is called immediately on program loading for development purposes
-        for (int i = 1; i <= 5; i++) {
-            addItem(createTaskItem());
-        }
-        for (int i = 1; i <= 5; i++) {
-            Task.COUNT++;
-            TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "Next Week " + Task.COUNT,
-                    makeDetails(Task.COUNT), TaskType.NEXTWEEK);
-            addItem(item);
-        }
-        for (int i = 1; i <= 5; i++) {
-            Task.COUNT++;
-            TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "This month " + Task.COUNT,
-                    makeDetails(Task.COUNT), TaskType.THISMONTH);
-            addItem(item);
-        }
-        for (int i = 1; i <= 5; i++) {
-            Task.COUNT++;
-            TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "ARCHIVE " + Task.COUNT,
-                    makeDetails(Task.COUNT), TaskType.ARCHIVED);
-            addItem(item);
-        }
-    }
+//
+//    static {
+//        // Add some sample items.
+//        // This is called immediately on program loading for development purposes
+//        for (int i = 1; i <= 5; i++) {
+//            addItem(createTaskItem());
+//        }
+//        for (int i = 1; i <= 5; i++) {
+//            Task.COUNT++;
+//            TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "Next Week " + Task.COUNT,
+//                    makeDetails(Task.COUNT), TaskType.NEXTWEEK);
+//            addItem(item);
+//        }
+//        for (int i = 1; i <= 5; i++) {
+//            Task.COUNT++;
+//            TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "This month " + Task.COUNT,
+//                    makeDetails(Task.COUNT), TaskType.THISMONTH);
+//            addItem(item);
+//        }
+//        for (int i = 1; i <= 5; i++) {
+//            Task.COUNT++;
+//            TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "ARCHIVE " + Task.COUNT,
+//                    makeDetails(Task.COUNT), TaskType.ARCHIVED);
+//            addItem(item);
+//        }
+//    }
 
     public static ArrayList<Task.TaskItem> getList(int i){
         if(i==1)
@@ -85,11 +82,50 @@ public class Task {
             return THISWEEK;
     }
 
+    public static void loadTasks(Context context) {
+
+        if (database == null) {
+            database = new DatabaseHandler(context);
+            Task.COUNT = Task.THISWEEK.size() + Task.NEXTWEEK.size() +
+                    Task.THISMONTH.size() + Task.ARCHIVE.size();
+        }
+
+        if (!database.loadTasks()) {
+            for (int i = 1; i <= 5; i++) {
+                TaskItem item = createTaskItem();
+                addItem(item);
+                //database.addTask(item);
+            }
+            for (int i = 1; i <= 5; i++) {
+                Task.COUNT++;
+                TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "Next Week " + Task.COUNT,
+                        makeDetails(Task.COUNT), TaskType.NEXTWEEK);
+                addItem(item);
+                //database.addTask(item);
+            }
+            for (int i = 1; i <= 5; i++) {
+                Task.COUNT++;
+                TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "This month " + Task.COUNT,
+                        makeDetails(Task.COUNT), TaskType.THISMONTH);
+                addItem(item);
+                //database.addTask(item);
+            }
+            for (int i = 1; i <= 5; i++) {
+                Task.COUNT++;
+                TaskItem item = new TaskItem(String.valueOf(Task.COUNT), "ARCHIVE " + Task.COUNT,
+                        makeDetails(Task.COUNT), TaskType.ARCHIVED);
+                addItem(item);
+                //database.addTask(item);
+            }
+        }
+    }
+
     // Helpful!
-    public static void addItemBack(TaskItem item, int pos) {
+    public static void addItemBack(TaskItem item, int position) {
         if(Integer.parseInt(item.id)<Task.COUNT) {
             // this is the reason we need the pos value stored in the TaskItem.
-            Task.getList(item.getTaskTypeValue()).add(pos, item);
+            Task.getList(item.getTaskTypeValue()).add(position, item);
+            //database.addTask(item);
         }
         else
             addItem(item);
@@ -97,6 +133,7 @@ public class Task {
     public static void addItem(TaskItem item) {
         ITEM_MAP.put(item.id, item);
         Task.getList(item.getTaskType().getValue()).add(item);
+        //database.addTask(item);
     }
 
     public static void removeItem(TaskItem item){
@@ -107,6 +144,7 @@ public class Task {
             for (int thisTask = 0; thisTask < Task.getList(thisList).size(); thisTask++) {
                 if(Task.getList(thisList).get(thisTask).id.equals(item.id)) {
                     Task.getList(thisList).remove(thisTask);
+                    //database.deleteTask(item);
                     thisTask--;
                 }
             }
@@ -119,21 +157,28 @@ public class Task {
             if (Task.getList(item.getTaskTypeValue()).get(i).id.equals(item.id)) {
                 Task.getList(item.getTaskTypeValue()).set(i,item);
                 ITEM_MAP.put(item.id, item);
+                //database.updateTask(item);
                 return;
             }
         }
         // else, removes item from all lists and adds item back in at end of list specified by item
         Task.removeItem(item);
         Task.addItem(item);
+        //database.addTask(item);
     }
 
     public static void saveItem(TaskItem item){
         if(item.isNewTaskType()){
             removeItem(ITEM_MAP.get(item.id));
+            //database.deleteTask(ITEM_MAP.get(item.id));
             item.saved();
             if(item.getTaskType() == TaskType.ARCHIVED)
-                item.setTaskComplete();
-            addItem(item);}
+                item.setEdited(true);
+            item.setCompletionTime(System.currentTimeMillis());
+            item.setTaskComplete(true);
+            addItem(item);
+            //database.addTask(item);
+        }
         else if(item.isEdited()){
             item.saved();
             updateItemListing(item);
@@ -177,7 +222,7 @@ public class Task {
 
     public static class TaskItem implements Serializable, Parcelable{
 
-        public final String id;
+        public String id;
         private String name;
         private String details;
         private long creationTime;
@@ -187,7 +232,11 @@ public class Task {
         private boolean edited;
         private boolean newTaskType;
 
-        private TaskItem(String id, String name, String details, TaskType taskType) {
+        TaskItem() {
+
+        }
+
+        public TaskItem(String id, String name, String details, TaskType taskType) {
             this.id = id;
             this.name = name;
             this.details = details;
@@ -199,28 +248,48 @@ public class Task {
             this.newTaskType = false;
         }
 
-        public String getName(){
-            return this.name;}
-        public String getDetails(){
-            return this.details;}
-        public int getTaskTypeValue(){
-            return this.taskType.value;}
-        public TaskType getTaskType(){
-            return this.taskType;}
+        String getID() {
+            return this.id;
+        }
+
+        void setID(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return this.name;
+        }
 
         public void setName(String name) {
             this.name = name;
         }
-        public void setDetails(String details){
-            this.edited = true;
-            this.details = details;}
 
-        public void setTaskType(TaskType taskType){
+        public String getDetails() {
+            return this.details;
+        }
+
+        public int getTaskTypeValue() {
+            return this.taskType.value;
+        }
+
+
+        public void setDetails(String details) {
+            this.edited = true;
+            this.details = details;
+        }
+
+        public TaskType getTaskType() {
+            return this.taskType;
+        }
+
+        public void setTaskType(TaskType taskType) {
             if(this.taskType != taskType){
                 this.edited = true;
                 this.newTaskType = true;
             }
-            this.taskType = taskType;}
+            this.taskType = taskType;
+        }
+
         public void setTaskType(int i){
             if(this.taskType.value != i){
                 this.edited = true;
@@ -233,24 +302,45 @@ public class Task {
                     this.taskType = TaskType.THISMONTH;
             }}
 
-        public void setTaskComplete(){
-            this.edited = true;
-            this.completionTime = System.currentTimeMillis();
-            this.taskComplete = true;}
-        public boolean getTaskComplete(){
-            return this.taskComplete;}
+        public boolean getTaskComplete() {
+            return this.taskComplete;
+        }
 
-        public long getCompletionTime(){
-            return this.completionTime;}
+        public void setTaskComplete(boolean taskComplete) {
+            this.taskComplete = taskComplete;
+        }
 
-        public long getCreationTime(){
-            return this.creationTime;}
+        public long getCompletionTime() {
+            return this.completionTime;
+        }
 
-        private boolean isEdited(){
-            return this.edited;}
+        void setCompletionTime(long completionTime) {
+            this.completionTime = completionTime;
+        }
 
-        private boolean isNewTaskType(){
-            return this.newTaskType;}
+        public long getCreationTime() {
+            return this.creationTime;
+        }
+
+        void setCreationTime(long creationTime) {
+            this.creationTime = creationTime;
+        }
+
+        boolean isEdited() {
+            return this.edited;
+        }
+
+        void setEdited(boolean edited) {
+            this.edited = edited;
+        }
+
+        boolean isNewTaskType() {
+            return this.newTaskType;
+        }
+
+        void setNewTaskType(boolean newTaskType) {
+            this.newTaskType = newTaskType;
+        }
 
         private void saved(){
             this.newTaskType = false;
@@ -310,5 +400,7 @@ public class Task {
                 return new TaskItem[size];
             }
         };
+
+
     }
 }
